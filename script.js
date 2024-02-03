079bdc2f9277cd940dff2f339d7f5b0232fade28
BriannaBarkema <145512323+BriannaBarkema@users.noreply.github.com>
2024-02-03 18:18:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let article = "some dummy value";
-
-
-
-
-
+let article = 'El Salvador voters stand poised to re-elect their current president and self-styled "world’s coolest dictator" Nayib Bukele in a landslide victory after he rehabilitated his country’s crime-ridden reputation.'
+const URL = "PARser.com/analyze"
 function analyze() {
-	document.getElementById("analyze").innerHTML = "Analyzing...";
 	// query the backend
+	document.getElementById("analyze").innerHTML = "Analyzing...";
+	fetch(URL, {
+		method: "POST",
+		body: JSON.stringify({
+			article: article
+		}),
+		headers: {
+			"Content-type": "application/json; charset-UTF-8"
+		}
+	}).then((response) => response.json())
+	  .then((json) => console.log(json));
 }
 
 document.addEventListener('DOMContentLoaded', function() {
