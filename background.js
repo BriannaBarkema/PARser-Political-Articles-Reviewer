d73b456ac705860f5e3aa83a9c2ed18a8b25a914
Maximo Librandi <maximolibrandi@hotmail.com>
2024-02-03 14:44:50 -0500

    Add basic plugin

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id},
+      function: () => {
+        alert(getSelection().toString());
+      }
+    });
+});
