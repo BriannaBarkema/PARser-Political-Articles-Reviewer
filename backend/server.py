4b274351e2f14e576117ba23fef68fc012c1f38b
Maximo Librandi <maximolibrandi@hotmail.com>
2024-02-03 17:23:40 -0500

    merged APIs

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,17 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 from flask_cors import CORS, cross_origin
 from gpt import analyze_misinformation_and_bias
 
 app = Flask(__name__)
 CORS(app, support_credentials=True) # Enable CORS for the API, allowing requests from any origin
 
+# Get request with query parameters
+@app.route('/analyze', methods=['GET'])
+def analyze():
+    text = request.args.get('text')
+    return render_template('analysis.html', text=text)
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/process', methods=['POST'])
 def analyze():
     if request.method == 'POST':
         # they should have sent a json with one key, 'article', which is the article to be analyzed
@@ -16,4 +21,4 @@ def analyze():
         return jsonify(result)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
