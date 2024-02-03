62646db9ff3b1aeccb78ac960b75e848516d2f87
Maximo Librandi <maximolibrandi@hotmail.com>
2024-02-03 17:11:35 -0500

    update plugin funcionality

--- background.js
+++ background.js
@@ -2,7 +2,9 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
       target: {tabId: tab.id},
       function: () => {
-        alert(getSelection().toString());
+        // Get the selected text and open a new tab with the text on the URL
+        new_corpus = getSelection().toString();        
+        window.open('http://localhost:5000/analyze?text=' + new_corpus, '_blank');
       }
     });
 });
