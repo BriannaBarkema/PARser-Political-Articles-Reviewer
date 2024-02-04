f344bc2cf8f29c85b611d717c71f9e6dee8bd852
BriannaBarkema <145512323+BriannaBarkema@users.noreply.github.com>
2024-02-03 20:12:49 -0500

    Latest Version

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const record=await res.json();
-    document.getElementById("date").innerHTML=record.data[0].date;
-    document.getElementById("areaName").innerHTML=record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+let article = 'El Salvador voters stand poised to re-elect their current president and self-styled "world’s coolest dictator" Nayib Bukele in a landslide victory after he rehabilitated his country’s crime-ridden reputation.'
+const URL = "PARser.com/analyze"
+function analyze() {
+	// query the backend
+	document.getElementById("analyze").innerHTML = "Analyzing...";
+	fetch(URL, {
+		method: "POST",
+		body: JSON.stringify({
+			article: article
+		}),
+		headers: {
+			"Content-type": "application/json; charset-UTF-8"
+		}
+	}).then((response) => response.json())
+	  .then((json) => console.log(json));
 }
-fetchData();
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+ document.getElementById("analyze").addEventListener('click', analyze, false);
+}, false)
+
+
